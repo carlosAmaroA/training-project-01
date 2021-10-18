62070caffdcfe43858de5d0e8c5ba2fff50d432c
carlos alberto <noobgaler1016@gmail.com>
2021-10-18 03:10:31 -0300

    Componentes de classe stateful

--- src/App.js
+++ src/App.js
@@ -3,21 +3,41 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //adiciona o this da classe no metodo handlePClick
+    this.handlePclick = this.handlePClick.bind(this);
+    this.state = {
+      name: "Otavio Miranda",
+      counter: 0,
+    };
+  }
+
+  handlePClick() {
+    this.setState({ name: "Júnior" });
+  }
+
+  handleAClick = (e) => {
+    e.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
+
   render() {
+    const { name, counter } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p onClick={this.handlePclick}>{name}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={this.handleAClick}
           >
-            Learn React
+            {counter}
           </a>
         </header>
       </div>
