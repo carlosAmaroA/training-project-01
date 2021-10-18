888df6473086eb8bbd8e3e2e69ea917c77a2dd18
carlos alberto <noobgaler1016@gmail.com>
2021-10-18 19:08:31 -0300

    state com arrays

--- src/App.js
+++ src/App.js
@@ -3,43 +3,24 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    //adiciona o this da classe no metodo handlePClick
-    this.handlePclick = this.handlePClick.bind(this);
-    this.state = {
-      name: "Otavio Miranda",
-      counter: 0,
-    };
-  }
-
-  handlePClick() {
-    this.setState({ name: "Júnior" });
-  }
-
-  handleAClick = (e) => {
-    e.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+  state = {
+    posts: [
+      { id: 1, title: "Titulo 1", body: "body 1" },
+      { id: 2, title: "Titulo 2", body: "body 2" },
+      { id: 3, title: "Titulo 3", body: "body 3" },
+    ],
   };
 
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePclick}>{name}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            onClick={this.handleAClick}
-          >
-            {counter}
-          </a>
-        </header>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
