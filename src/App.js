841950f5a4a86098b738b06ba8fbe03cb3343584
carlos alberto <noobgaler1016@gmail.com>
2021-10-19 00:54:08 -0300

    life-cycles

--- src/App.js
+++ src/App.js
@@ -4,17 +4,38 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
+    count: 0,
     posts: [
       { id: 1, title: "Titulo 1", body: "body 1" },
       { id: 2, title: "Titulo 2", body: "body 2" },
       { id: 3, title: "Titulo 3", body: "body 3" },
     ],
   };
+  timeOutUpdate = null;
+  handleTimeout = () => {
+    const { count } = this.state;
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ count: count + 1 });
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
 
   render() {
-    const { posts } = this.state;
+    const { posts, count } = this.state;
     return (
       <div className="App">
+        <h1>{count}</h1>
         {posts.map((post) => (
           <div key={post.id}>
             <h1>{post.title}</h1>
